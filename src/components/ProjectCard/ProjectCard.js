51b142963e2af49cffb48156d81b0cf19ea69b09
bondastefana <71835217+bondastefana@users.noreply.github.com>
2021-02-25 14:38:16 +0200

    Portfolio page

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,107 @@
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Box,
+  Typography,
+  Button,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+
+const useStyles = makeStyles((theme) => ({
+  cardContainer: {
+    margin: '10px 5px',
+    backgroundColor: theme.palette.background.footer,
+  },
+  cardImage: {
+    [theme.breakpoints.up('xs')]: {
+      height: '180px',
+    },
+    [theme.breakpoints.up('sm')]: {
+      height: '280px',
+    },
+    [theme.breakpoints.up('md')]: {
+      height: '230px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      height: '400px',
+    },
+  },
+  titleContainer: {
+    fontSize: '30px',
+  },
+  cardDescription: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    fontSize: '15px',
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '17px',
+      minHeight: '140px',
+    },
+  },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+}))
+
+function ProjectCard(props) {
+  const { imagePath, title, description, url } = props
+  const mobile = useMediaQuery('(max-width:500px)')
+
+  const {
+    cardContainer,
+    cardImage,
+    titleContainer,
+    cardDescription,
+    buttonContainer,
+  } = useStyles()
+  return (
+    <Card className={cardContainer}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          image={imagePath}
+          title="Contemplative Reptile"
+          className={cardImage}
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="h2"
+            className={titleContainer}
+          >
+            {title}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="secondary"
+            component="p"
+            className={cardDescription}
+          >
+            {description}
+          </Typography>
+          <Box className={buttonContainer}>
+            <Button
+              size={mobile ? 'medium' : 'large'}
+              color="primary"
+              variant="outlined"
+              onClick={() => {
+                window.open(url, '_blank')
+              }}
+            >
+              See project
+            </Button>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
+
+export default ProjectCard
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,5 +1,7 @@
 import { Grid, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import projectsData from '../../components/ProjectCard/staticData.js'
+import ProjectCard from '../../components/ProjectCard/ProjectCard.js'
 
 const useStyles = makeStyles((theme) => ({
   portfolioContainer: {
@@ -8,18 +10,64 @@ const useStyles = makeStyles((theme) => ({
       padding: '20px 100px',
     },
   },
+  titlePadding: {
+    paddingLeft: theme.spacing(4),
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '17px',
+    },
+  },
+  title: {
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '17px',
+    },
+  },
+  titleContainer: {
+    [theme.breakpoints.up('sm')]: {
+      padding: theme.spacing(3),
+    },
+  },
 }))
 
-function Portfolio() {
-  const code = 'if(youWant() === true) { youCan();} else{ youCant(); }'
-  const { portfolioContainer, title } = useStyles()
+function Portfolio(props) {
+  const {
+    portfolioContainer,
+    title,
+    titlePadding,
+    titleContainer,
+  } = useStyles()
+
   return (
     <Grid container className={portfolioContainer}>
-      <Grid item xs={12}>
+      <Grid item xs={12} className={titleContainer}>
         <Typography variant="h5" className={title}>
-          {code}
+          {'if (youWant() === true) {'}
+        </Typography>
+        <Typography variant="h5" className={titlePadding}>
+          {'youCan();'}
+        </Typography>
+        <Typography variant="h5" className={title}>
+          {'} else {'}
+        </Typography>
+        <Typography variant="h5" className={titlePadding}>
+          {'youCant();'}
+        </Typography>
+        <Typography variant="h5" className={title}>
+          {'}'}
         </Typography>
       </Grid>
+      {projectsData.map((project, index) => {
+        return (
+          <Grid item xs={12} md={6}>
+            <ProjectCard
+              key={index}
+              imagePath={project.imagePath}
+              title={project.title}
+              description={project.description}
+              url={project.url}
+            />
+          </Grid>
+        )
+      })}
     </Grid>
   )
 }
