dfc79cd569589d2edef1c662737a5a70bd7af061
stefanabonda <stefana.bonda@pronovix.com>
2021-05-18 13:57:34 +0300

    review

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,24 +6,25 @@ import Divider from '@material-ui/core/Divider'
 import Paper from '@material-ui/core/Paper'
 import Icons from '../../components/Icons/Icons.js'
 import { useState, useEffect } from 'react'
+import profilePicture from '../../assets/profilePicture.jpg'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   homeContainer: {
     padding: '20px 30px',
     [theme.breakpoints.up('sm')]: {
-      padding: '45px 50px',
+      padding: '45px 50px'
     },
     [theme.breakpoints.up('md')]: {
-      padding: '45px 80px',
-    },
+      padding: '45px 80px'
+    }
   },
   avatar: {
     width: '100%',
     height: '100%',
     [theme.breakpoints.up('lg')]: {
       width: '75%',
-      height: '75%',
-    },
+      height: '75%'
+    }
   },
   avatarContainer: {
     padding: '20px 10px',
@@ -31,62 +32,62 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     [theme.breakpoints.up('sm')]: {
       paddingTop: '50px',
-      paddingLeft: '30px',
-    },
+      paddingLeft: '30px'
+    }
   },
   title: {
     display: 'flex',
     justifyContent: 'center',
     [theme.breakpoints.up('sm')]: {
       paddingTop: '20px',
-      paddingBottom: '25px',
+      paddingBottom: '25px'
     },
     [theme.breakpoints.up('md')]: {
-      fontSize: '45px',
-    },
+      fontSize: '45px'
+    }
   },
   aboutMe: {
     marginTop: '20px',
     textAlign: 'center',
     [theme.breakpoints.up('md')]: {
-      fontSize: '25px',
-    },
+      fontSize: '25px'
+    }
   },
   quoteContainer: {
-    paddingTop: '50px',
+    paddingTop: '50px'
   },
   quote: {
     padding: '20px 0',
     fontFamily: 'Poppins',
-    fontWeight: 100,
+    fontWeight: 100
   },
   dividerContainer: {
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   divider: {
-    width: '50%',
+    width: '50%'
   },
   iconsContainer: {
     display: 'flex',
     justifyContent: 'center',
-    padding: '40px',
+    padding: '40px'
   },
   paper: {
     padding: '6px 16px',
     [theme.breakpoints.up('md')]: {
-      padding: '6px 40px',
-    },
+      padding: '6px 40px'
+    }
   },
   isVisible: {
-    display: 'inline',
+    display: 'inline'
   },
   notVisible: {
-    visibility: 'hidden',
-  },
+    visibility: 'hidden'
+  }
 }))
 
-function Home(props) {
+function Home (props) {
   const [toggle, setToggle] = useState(false)
 
   const {
@@ -101,7 +102,7 @@ function Home(props) {
     aboutMe,
     paper,
     isVisible,
-    notVisible,
+    notVisible
   } = useStyles()
 
   useEffect(() => {
@@ -114,7 +115,7 @@ function Home(props) {
     <Grid container className={homeContainer}>
       <Grid item xs={12} sm={6} md={8} lg={8}>
         <Paper elevation={3} className={paper}>
-          <Typography variant="h5" className={title}>
+          <Typography variant='h5' className={title}>
             Hey! I'm Stefana
             <span className={toggle ? isVisible : notVisible}>_</span>
           </Typography>
@@ -132,20 +133,20 @@ function Home(props) {
       <Grid item xs={12} sm={6} md={4} lg={4}>
         <div className={avatarContainer}>
           <Avatar
-            alt="profile-picture"
-            src="https://media-exp1.licdn.com/dms/image/C4D03AQGTSRQ9UWSO3g/profile-displayphoto-shrink_800_800/0/1596815606145?e=1619654400&v=beta&t=PiBUP8K_EBrE5poGq2hh099Ay5KGldeFTyVLTldS1yc"
+            alt='profile-picture'
+            src={profilePicture}
             className={avatar}
           />
         </div>
       </Grid>
 
       <Grid item xs={12} sm={12} md={12} className={quoteContainer}>
-        <Divider variant="middle" />
-        <Typography align="center" variant="h6" className={quote}>
+        <Divider variant='middle' />
+        <Typography align='center' variant='h6' className={quote}>
           Make everyday a little less ordinary!
         </Typography>
         <div className={dividerContainer}>
-          <Divider variant="middle" className={divider} />
+          <Divider variant='middle' className={divider} />
         </div>
       </Grid>
 
